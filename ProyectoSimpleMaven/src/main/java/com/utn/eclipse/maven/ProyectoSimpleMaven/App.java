e411e3b75c5f363164c805ff5d70a1220bfc2e20
19shosho94 <38757254+19shosho94@users.noreply.github.com>
2019-01-10 18:03:35 -0500

    Subida
    Subir Archivos GB

---
+++ ProyectoSimpleMaven/src/main/java/com/utn/eclipse/maven/ProyectoSimpleMaven/App.java
@@ -0,0 +1,13 @@
+package com.utn.eclipse.maven.ProyectoSimpleMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Geovanny Beltran" );
+    }
+}
